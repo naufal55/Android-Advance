747cfb513d32942fcac80355e66b57d72da758cc
naufal55 <moehhammadnofall55@gmail.com>
2021-09-17 20:34:42 +0700

    update project fragment communicate

--- FragmentCommunicate/app/src/main/java/com/example/fragmentcommunicate/SimpleFragment.java
+++ FragmentCommunicate/app/src/main/java/com/example/fragmentcommunicate/SimpleFragment.java
@@ -1,4 +1,4 @@
-package com.example.fragmentcommunicate;
+package com.example.FragmentCommunicate;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -9,18 +9,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioGroup;
-import android.widget.Switch;
 import android.widget.TextView;
 
+
+
 public class SimpleFragment extends Fragment {
-    private static final int YES = 0;
-    private static final int NO = 1;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final int NONE = 2;
     public int mRadioButtonChoice = NONE;
     OnFragmentInteractionListener mListener;
-    TextView textView;
+
+    private static final String CHOICE = "choice";
+
+    // TODO: Rename and change types of parameters
+
+    private static final int YES = 0;
+    private static final int NO = 1;
+
+
     public SimpleFragment() {
         // Required empty public constructor
+    }
+
+    public static SimpleFragment newInstance(int choice) {
+        SimpleFragment fragment = new SimpleFragment();
+        Bundle arguments = new Bundle();
+        arguments.putInt(CHOICE, choice);
+        fragment.setArguments(arguments);
+        return fragment;
     }
 
     interface OnFragmentInteractionListener {
@@ -41,47 +60,44 @@ public class SimpleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        // Inflate the layout for this fragment.
-        final View rootView = inflater.inflate(R.layout.fragment_simple,
-                container, false);
+        final View rootView =
+                inflater.inflate(R.layout.fragment_simple, container, false);
         final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
 
-        switch (mRadioButtonChoice){
-            case YES: // User chose "Yes."
-                textView.setText(R.string.yes_message);
-                mRadioButtonChoice = YES;
-                mListener.onRadioButtonChoice(YES);
-                break;
-            case NO: // User chose "No."
-                textView.setText(R.string.no_message);
-                mRadioButtonChoice = NO;
-                mListener.onRadioButtonChoice(NO);
-                break;
-            default: // No choice made.
-                mRadioButtonChoice = NONE;
-                mListener.onRadioButtonChoice(NONE);
-                break;
+        if (getArguments().containsKey(CHOICE)) {
+            // A choice was made, so get the choice.
+            mRadioButtonChoice = getArguments().getInt(CHOICE);
+            // Check the radio button choice.
+            if (mRadioButtonChoice != NONE) {
+                radioGroup.check
+                        (radioGroup.getChildAt(mRadioButtonChoice).getId());
+            }
         }
 
-        // Set the radioGroup onCheckedChanged listener.
+
+
         radioGroup.setOnCheckedChangeListener(
                 new RadioGroup.OnCheckedChangeListener() {
                     @Override
                     public void onCheckedChanged(RadioGroup group, int checkedId) {
                         View radioButton = radioGroup.findViewById(checkedId);
                         int index = radioGroup.indexOfChild(radioButton);
-                        textView =
+                        TextView textView =
                                 rootView.findViewById(R.id.fragment_header);
                         switch (index) {
                             case YES: // User chose "Yes".
                                 textView.setText(R.string.yes_message);
+                                mRadioButtonChoice = YES;
+                                mListener.onRadioButtonChoice(YES);
                                 break;
                             case NO: // User chose "No".
                                 textView.setText(R.string.no_message);
+                                mRadioButtonChoice = NO;
+                                mListener.onRadioButtonChoice(NO);
                                 break;
                             default: // No choice made.
-                                // Do nothing.
+                                mRadioButtonChoice = NONE;
+                                mListener.onRadioButtonChoice(NONE);
                                 break;
                         }
                     }
@@ -91,7 +107,4 @@ public class SimpleFragment extends Fragment {
         return rootView;
     }
 
-    public static SimpleFragment newInstance() {
-        return new SimpleFragment();
-    }
 }
